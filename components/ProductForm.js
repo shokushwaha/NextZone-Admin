3cfaf54cff1a2e70ef879c3d54a24b85a4a20736
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-05-09 17:01:12 +0530

    products-image

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -5,7 +5,8 @@ export default function ProductForm({
     _id,
     title: existingTitle,
     description: existingDescription,
-    price: existingPrice }) {
+    price: existingPrice,
+    images }) {
 
     const router = useRouter();
     const [title, setTitle] = useState(existingTitle || "");
@@ -31,6 +32,16 @@ export default function ProductForm({
     if (goToProducts) {
         router.push('/products');
     }
+
+
+
+    const uploadImages = async (e) => {
+        const files = e.target?.files;
+        if (files?.length > 0) {
+            const data = new FormData();
+            files.forEach(file => data.append('file' + file));
+        }
+    }
     return (
         <>
 
@@ -44,6 +55,21 @@ export default function ProductForm({
                     placeholder="Product Name"
                     value={title}
                     onChange={e => setTitle(e.target.value)} />
+
+
+                <label>
+                    Photos
+                </label>
+                <div className="mb-2">
+                    <label className="w-24 h-24 flex flex-col items-center justify-center text-gray-400 rounded-lg bg-gray-200 hover:bg-gray-300 hover:cursor-pointer "><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                    </svg>
+                        Upload
+                        <input type="file" className="hidden" onChange={uploadImages} />
+                    </label>
+                    {!images?.length && (<div>No photos to show </div>)}
+                </div>
+
 
                 <label >Description</label>
                 <textarea
