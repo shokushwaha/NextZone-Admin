70721c5474a3ec2a47ab8ab72da0e39f2f220133
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-05-10 16:42:36 +0530

    v1

--- components/ProductForm.jsx
+++ components/ProductForm.jsx
@@ -158,7 +158,11 @@ export default function ProductForm({
             />
             <button
                 type="submit"
-                className="btn-primary">
+                className="text-white px-2 py-1  flex gap-1 bg-green-500 rounded-md shadow-md hover:bg-green-600">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+                </svg>
+
                 Save
             </button>
         </form>
