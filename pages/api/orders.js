5032b45cd840429525d3dc6df96518491caae37b
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-05-13 21:46:21 +0530

    analytics updated

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -3,7 +3,10 @@ import { Order } from "@/models/Orders";
 import { isAdmin } from "./isAdmin"
 const handler = async (req, res) => {
     await mongooseConnect();
-    res.json(await Order.find());
+    const { method } = req;
+    if (method === "GET") {
+        res.json(await Order.find());
+    }
 }
 
 export default isAdmin(handler);
