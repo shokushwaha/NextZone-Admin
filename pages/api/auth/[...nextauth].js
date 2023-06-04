5381d896550362cae5ea538da171f860610fe392
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2023-06-04 19:08:50 +0530

    v4

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,7 @@ import md5 from "md5";
 const adminEmails = ["6f1370b01cbabc921b8e87272e2fec40", "0166a104631632ff61d8280a6a4bf9c2", "518950e28fee1b9ea3100a4f5790d8e3"];
 
 export const authOptions = {
-    secret: process.env.NEXTAUTH_SECRET,
+
     debug: true,
     providers: [
         GoogleProvider({
@@ -16,6 +16,7 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         }),
     ],
+    secret: process.env.NEXTAUTH_SECRET,
     adapter: MongoDBAdapter(clientPromise),
     callbacks: {
         session: async ({ session, user }) => {
